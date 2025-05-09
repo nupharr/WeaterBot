7e66f7a241755817f1c4f97bf0f674f9675c61ec
nupharr <ilyakubyshkin06@gmail.com>
2025-05-09 03:09:17 +0300

    09/05/2025

--- src/handlers.py
+++ src/handlers.py
@@ -4,7 +4,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 
-from src.database import append_user, check_user_exists, get_location
+from src.database import append_user, check_user_exists, get_location, delete_user
 from src.responces import get_weater
 from src.text import start_handler
 
@@ -19,7 +19,7 @@ class Reg(StatesGroup):
 async def command_start_handler(message: Message, state: FSMContext):
     if check_user_exists(message.from_user.id):
         await message.answer(
-            f"Добро пожаловать {message.from_user.full_name}!\nВаше местоположение - {get_location(message.from_user.id)["name"]}\nВоспользуйтесь командой /weather"
+            f"Добро пожаловать {message.from_user.full_name}!\nВаше местоположение - {get_location(message.from_user.id)["name"]}\nВоспользуйтесь командой /weather\nЕсли хотите удалить город воспользуйтесь /del_city"
         )
     else:
         await state.set_state(Reg.city)
@@ -45,3 +45,9 @@ async def command_help_handler(message: Message):
             get_location(message.from_user.id)["lon"],
         )
     )
+
+
+@router.message(Command("del_city"))
+async def command_delete_city(message: Message):
+    delete_user(message.from_user.id)
+    await message.answer("Город удалён!\nВоспользуйтесь командой /start")
